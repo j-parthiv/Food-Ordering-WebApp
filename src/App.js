05d845c1d125806d0fba86d6375e6d378ede6d04
j-parthiv <parthivjani9@gmail.com>
2022-12-28 11:57:09 +0100

    Added Meals Listing Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Fragment } from "react";
 import Header from './Components/Layout/Header'
+import Meals from "./Components/Meals/Meals";
 
 function App() {
   return (
     <fragment>
        <Header/>
+       <main>
+        <Meals/>
+       </main>
     </fragment>
   );
 }
